b251090a8673c61808c814957b425cc28463f7de
Maryem B <34491538+int-j@users.noreply.github.com>
2023-12-27 15:38:15 +0100

    Add files via upload

---
+++ C06/ft_sort_params.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbamou <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/07 09:03:45 by mbamou            #+#    #+#             */
+/*   Updated: 2021/10/13 17:51:17 by mbamou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include<unistd.h>
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' && (s1[i] == s2[i]))
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (++i < argc)
+	{
+		j = i;
+		while (++j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+				ft_swap(&argv[i], &argv[j]);
+		}
+	}
+	i = 0;
+	while (++i < argc)
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+	}
+}
